1aa2226379957f6f3debecd649496104e1d02e31
YanC Lin <yonalin1@gmail.com>
2018-08-11 05:32:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,4 +13,13 @@ function ternaryCheckCity(city) {
   return city ==='NYC'? 'Ok, sounds good.': 'No go.'
 }
 
-
+function switchOnCharmFromTip(tip) { 
+ switch (tip){
+   case 'generous':
+   return 'Thank you so much.';
+   case 'not as generous':
+   return 'Thank you.';
+   default:
+   return 'Bye.';
+ }
+}
