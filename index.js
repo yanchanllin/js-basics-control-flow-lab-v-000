f62e0c08840aad0e408d8c91027e5464cf016afb
YanC Lin <yonalin1@gmail.com>
2018-08-11 05:26:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet){
+  if (feet<= 400) {
+    return 'This one is on me!';
+  } else if (401 < feet && feet < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
+} 
